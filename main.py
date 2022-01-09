a11a5adf270ec296461cfad3b0f057f492445dc9
Thabata Pontes <tathabatapontes@hotmail.com>
2022-01-09 19:57:23 -0300

    Create spark application

--- main.py
+++ main.py
@@ -2,8 +2,10 @@
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+from pyspark.sql import SparkSession
+from pyspark.sql import SQLContext
 
-
+# https://towardsdatascience.com/create-your-first-etl-pipeline-in-apache-spark-and-python-ec3d12e2c169
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
@@ -11,6 +13,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    scSpark = SparkSession.builder.appName("reading csv").getOrCreate()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
