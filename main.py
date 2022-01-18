ff7beedcfbe5e0b658f802a795e3270d78e42dee
Thabata Pontes <tathabatapontes@hotmail.com>
2022-01-17 23:45:01 -0300

    Add connection to database

--- main.py
+++ main.py
@@ -8,9 +8,27 @@ def print_hi(name):
 
 
 if __name__ == '__main__':
-    scSpark = SparkSession.builder.appName("reading csv").getOrCreate()
+    spark = SparkSession.builder.appName("reading csv").getOrCreate()
 
     data_file = '/Users/thabata.pontes/Downloads/life_insurance.csv'
-    sdfData = scSpark.read.csv(data_file).cache()
+    sdfData = spark.read.csv(data_file).cache()
     print('Total Records = {}'.format(sdfData.count()))
     sdfData.show()
+
+    # set variable to be used to connect the database
+    database = "database-1.cg9pkmbnsltc.sa-east-1.rds.amazonaws.com"
+    table = sdfData
+    user = "thaapontes"
+    password = "t28230729"
+
+    # read table data into a spark dataframe
+    jdbcDF = spark.read.format("jdbc") \
+        .option("url", f"{database}") \
+        .option("dbtable", table) \
+        .option("user", user) \
+        .option("password", password) \
+        .option("driver", "com.microsoft.sqlserver.jdbc.SQLServerDriver") \
+        .load()
+
+    # show the data loaded into dataframe
+    jdbcDF.show()
