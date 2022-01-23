1e3838d4234bd862a0f9340eafee3ebb00337654
Thabata Pontes <tathabatapontes@hotmail.com>
2022-01-23 20:54:40 -0300

    Add basic transformations to one of the dataframes

--- main.py
+++ main.py
@@ -1,34 +1,61 @@
+import pyspark
 from pyspark.sql import SparkSession
 from pyspark.sql import SQLContext
+from pyspark.sql.functions import when, col
 
-# https://towardsdatascience.com/create-your-first-etl-pipeline-in-apache-spark-and-python-ec3d12e2c169
+
 def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
 
 if __name__ == '__main__':
-    spark = SparkSession.builder.appName("reading csv").getOrCreate()
+    # Create a session on a local master
+    spark = SparkSession.builder\
+        .appName("reading csv")\
+        .master("local")\
+        .getOrCreate()
 
+    # Read CSV file - 1
     data_file = '/Users/thabata.pontes/Downloads/life_insurance.csv'
-    sdfData = spark.read.csv(data_file).cache()
-    print('Total Records = {}'.format(sdfData.count()))
-    sdfData.show()
+    insuranceDataFrame = spark.read.csv(data_file).cache()
+
+    # Read CSV file - 2
+    pjDataFrame = spark.read\
+        .format("csv")\
+        .option("header", "true")\
+        .load("/Users/thabata.pontes/Downloads/pj.csv")
+
+    # How many records in the df and its content
+    print('Total Records = {}'.format(insuranceDataFrame.count()))
+    insuranceDataFrame.show(5)
+    pjDataFrame.show(5)
+
+    # Transforming the data
+    pjTransformed = pjDataFrame\
+        .withColumn("is_aware", when(col("current_stage") == "aware", 1).otherwise(0))\
+        .groupBy('is_aware').count()
+
+    pjTransformed.show(5)
+
+    '''
+    Next step: save file to database
+    '''
+    # TODO get credentials from aws RDS
 
     # set variable to be used to connect the database
-    database = "database-1.cg9pkmbnsltc.sa-east-1.rds.amazonaws.com"
-    table = sdfData
-    user = "thaapontes"
-    password = "t28230729"
+    # database = "database-1.cg9pkmbnsltc.sa-east-1.rds.amazonaws.com"
+    # table = sdfData
+    # user = "thaapontes"
+    # password = "t28230729"
 
     # read table data into a spark dataframe
-    jdbcDF = spark.read.format("jdbc") \
-        .option("url", f"{database}") \
-        .option("dbtable", table) \
-        .option("user", user) \
-        .option("password", password) \
-        .option("driver", "com.microsoft.sqlserver.jdbc.SQLServerDriver") \
-        .load()
+    # jdbcDF = spark.read.format("jdbc") \
+    #     .option("url", f"{database}") \
+    #     .option("dbtable", table) \
+    #     .option("user", user) \
+    #     .option("password", password) \
+    #     .option("driver", "com.microsoft.sqlserver.jdbc.SQLServerDriver") \
+    #     .load()
 
     # show the data loaded into dataframe
-    jdbcDF.show()
+    # jdbcDF.show()
