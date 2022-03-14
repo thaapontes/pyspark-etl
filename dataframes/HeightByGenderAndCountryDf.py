523ad6c781fdf341e600f095f377862ab4dba120
Thabata Pontes <tathabatapontes@hotmail.com>
2022-03-13 21:37:41 -0300

    Move dataframe from main to proper file

---
+++ dataframes/HeightByGenderAndCountryDf.py
@@ -0,0 +1,9 @@
+from pyspark import SparkContext
+
+"""
+    Csv example from: https://www.kaggle.com/datasets
+"""
+def height_by_gender_and_country(sc: SparkContext):
+    data_file_path = '/Users/thabata.pontes/Desktop/PySparkETL/kaggle_csvs/Height_of_Male_and_Female_by_Country_2022.csv'
+    df = sc.read.option("header", "true").csv(data_file_path).cache()
+    df.show(5)
