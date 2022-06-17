e39d749d4eb07e655478091d1b20b4b4690e6857
Thabata Pontes <tathabatapontes@hotmail.com>
2022-06-17 00:19:01 -0300

    Add read from file stream

--- main.py
+++ main.py
@@ -94,7 +94,18 @@ if __name__ == '__main__':
 
     '''
     Read lines from file stream
+    reference: Spark in action ch10
+    df = spark.readStream.format("text").load("/tmp/")
+
+    query = df.writeStream.outputMode("append")\
+        .format("console")\
+        .option("truncate", False)\
+        .option("numRows", 3)\
+        .start()
+
+    query.awaitTermination()
     '''
+
     # chapter 08: database
 
     '''
