828d92ee0ad4cb890477f6abdd1bdbf5c15c057c
Thabata Pontes <tathabatapontes@hotmail.com>
2022-07-04 23:35:17 -0300

    Add option inferSchema to json ingestion

--- ingested_dataframes/thailand_public_train.py
+++ ingested_dataframes/thailand_public_train.py
@@ -10,12 +10,14 @@ from pyspark.sql import SparkSession
 __all__ = ["thailand_public"]
 
 
-# JSON INGESTION WITHOUT SCHEMA
 def thailand_public(spark: SparkSession):
     current_dir = "/Users/thabata.pontes/Desktop/PySparkETL/kaggle_files/"
     relative_path = "thailand_public_train.json"
     absolute_file_path = os.path.join(current_dir, relative_path)
-    df = spark.read.format("json").option("multiline","true").load(absolute_file_path)
+    # JSON INGESTION WITHOUT SCHEMA
+    df = spark.read.format("json").option("multiline", "true").load(absolute_file_path)
+    # JSON INGESTION WITH SCHEMA
+    # df = spark.read.format("json").option("inferSchema", "true").load(absolute_file_path)
 
     print("Thailand public train DataFrame: ")
     df.show(5)
