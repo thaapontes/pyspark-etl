5fb00027bef9485fff24875106e08f105e803bc2
Thabata Pontes <tathabatapontes@hotmail.com>
2022-08-02 22:23:01 -0300

    Add regex_extract column

--- transformed_dataframes/height_grouped.py
+++ transformed_dataframes/height_grouped.py
@@ -1,4 +1,5 @@
 from pyspark.sql import (SparkSession, functions as F)
+from pyspark.sql.functions import regexp_extract
 from pyspark.sql.types import StructType, StructField, IntegerType
 
 from height_by_gender_and_country_df import height_by_gender_and_country
@@ -13,8 +14,10 @@ def height_transformed(spark: SparkSession):
     """
     df = height_by_gender_and_country(spark)
     df_using_functions = df.withColumnRenamed("Country Name", "country_name").drop("Male Height in Ft").withColumn(
-        "random_column", F.when(F.col("Female Height in Cm") > 170.0, "tall").otherwise("short"))
+        "random_column", F.when(F.col("Female Height in Cm") > 170.0, "tall").otherwise("short")).withColumn(
+        "regex_extract", regexp_extract(F.col("country_name"), "Brazil|Iceland", 0))
 
+    print("Height by gender and country transformed DataFrame: ")
     df_using_functions.show(5)
 
     data_array = [[1, 2], [2, 4], [3, 6], [4, 7]]
