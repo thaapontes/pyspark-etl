e84d079c29661175bfd5aee3ffe18f2a492ff5ef
Thabata Pontes <tathabatapontes@hotmail.com>
2022-08-09 22:04:46 -0300

    Deals with null values

--- transformed_dataframes/movies_dates_structs_nulls.py
+++ transformed_dataframes/movies_dates_structs_nulls.py
@@ -1,12 +1,13 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, to_date, current_date, current_timestamp, datediff, struct, split, array_contains
+from pyspark.sql.functions import col, to_date, current_date, current_timestamp, datediff, struct, split, \
+    array_contains, coalesce
 
 from movies_df import movies
 
-__all__ = ["movies_dates"]
+__all__ = ["movies_dates_structs_nulls"]
 
 
-def movies_dates(spark: SparkSession):
+def movies_dates_structs_nulls(spark: SparkSession):
     df = movies(spark)
     df_dates = df.withColumn("release_date", to_date(col("Release_Date"), "d-MMM-yy")) \
         .withColumn("today", current_date()) \
@@ -15,11 +16,17 @@ def movies_dates(spark: SparkSession):
         .withColumn("profit_structure", struct(col("US_Gross"), col("Worldwide_Gross"))) \
         .withColumn("us_profit", col("profit_structure").getField("US_Gross")) \
         .withColumn("title_words", split(col("Title"), " |,")) \
-        .withColumn("love_in_title", array_contains(col("title_words"), "Love"))
+        .withColumn("love_in_title", array_contains(col("title_words"), "Love")) \
+        .withColumn("final_rating", coalesce(col("Rotten_Tomatoes_Rating"), col("IMDB_Rating") * 10)) \
+        .orderBy(col("final_rating").desc_nulls_last()) \
+        .na.fill(0, ["IMDB_Rating", "Rotten_Tomatoes_Rating"]) \
+        .na.drop()  # remove rows containing nulls
 
     '''
     How to deal with multiple date formats?
     Parse the df multiple times and union the small dfs afterwards
+    How to deal with null values?
+    Use coalesce between columns, .na.fill to fulfill the null values with another value, .na.drop() to remove nulls
     '''
     print("Movies Df transformed")
     df_dates.show(5)
