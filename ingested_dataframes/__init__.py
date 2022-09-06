4ba8bcbbef2263799f0558b8f427af15398b07a1
Thabata Pontes <tathabatapontes@hotmail.com>
2022-09-05 22:34:16 -0300

    Add rdd second example

--- ingested_dataframes/__init__.py
+++ ingested_dataframes/__init__.py
@@ -2,7 +2,9 @@ from pyspark.sql import SparkSession
 
 from cars_df import *
 from covid_df import *
+from covid_rdd import *
 from create_df_from_row import *
+from create_rdd_from_range import *
 from enforcing_schema import *
 from height_by_gender_and_country_df import *
 from movies_df import *
@@ -14,11 +16,13 @@ from thailand_public_train import *
 def __all__(spark: SparkSession):
     list_all_functions = (ge_cars(spark),
                           labels_covid(spark),
+                          labels_covid_rdd(spark),
                           create_dataframe_from_row(spark),
                           enforcing_cars_schema(spark),
                           height_by_gender_and_country(spark),
                           movies(spark),
                           create_dataframe_from_array(spark),
+                          rdd(spark),
                           smartphones_dataframe(spark),
                           spotify_songs(spark),
                           thailand_public(spark))
