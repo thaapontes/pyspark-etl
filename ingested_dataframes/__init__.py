246279c17c88529ae900c54dfbe0ce25cd93312d
Thabata Pontes <tathabatapontes@hotmail.com>
2022-09-25 16:08:37 -0300

    Add next steps file

--- ingested_dataframes/__init__.py
+++ ingested_dataframes/__init__.py
@@ -4,6 +4,7 @@ from cars_df import *
 from covid_df import *
 from covid_rdd import *
 from create_df_from_row import *
+from create_empty_rdd import *
 from create_rdd_from_range import *
 from enforcing_schema import *
 from height_by_gender_and_country_df import *
@@ -18,11 +19,12 @@ def __all__(spark: SparkSession):
                           labels_covid(spark),
                           labels_covid_rdd(spark),
                           create_dataframe_from_row(spark),
+                          empty_rdd(spark),
+                          rdd(spark),
                           enforcing_cars_schema(spark),
                           height_by_gender_and_country(spark),
                           movies(spark),
                           create_dataframe_from_array(spark),
-                          rdd(spark),
                           smartphones_dataframe(spark),
                           spotify_songs(spark),
                           thailand_public(spark))
